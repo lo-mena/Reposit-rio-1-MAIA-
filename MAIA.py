210acfef66f6884624b6ae2385f559570f270d06
lo-mena <164680928+lo-mena@users.noreply.github.com>
2024-03-24 21:17:00 -0300

    Create MAIA.py

---
+++ MAIA.py
@@ -0,0 +1,7 @@
+a = 20
+b = 40
+dobro = b * 2
+c = b + a
+print(f'a soma de {b} mais {a} é igual {c} e o seu tipo é {type(c)}')
+print(f'o dobro de {b} é igual {dobro} e o seu tipo é {type(dobro)}')
+print(f'o dobro de {b} é igual {b * 2} e o seu tipo é {type(b * 2)}')
